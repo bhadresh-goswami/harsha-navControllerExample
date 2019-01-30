c474f7267742b7dfec29d369760e4ca97240205c
tops-bhadresh <tops.bhadresh@gmail.com>
2019-01-29 22:48:11 -0800

    upload

--- navControllerExample/ViewController.swift
+++ navControllerExample/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func TapToGoView3(_ sender: UIButton) {
+        
+        let view3Scene = self.storyboard?.instantiateViewController(withIdentifier: "view3Scene") as! View3ViewController
+        
+        self.navigationController?.pushViewController(view3Scene, animated: true)
+        
+        
+        
+    }
+    @IBAction func TapToGoView4(_ sender: UIButton) {
+        
+        
+        let view4Scene = self.storyboard?.instantiateViewController(withIdentifier: "view4Scene") as! View4ViewController
+        
+        self.present(view4Scene, animated: true, completion: nil)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
